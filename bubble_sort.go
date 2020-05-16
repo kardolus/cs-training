0f8b56b4fa0af3d081a1d28a81c5cba59d5633c8
Guillermo Kardolus <kardolus@gmail.com>
2020-05-16 11:24:28 -0400

    Create bubble_sort.go

---
+++ bubble_sort.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {  
+  var unsorted = []int{5, 4, 2, 8, 4, 7, 10, 1, 3}
+  fmt.Println("unsorted:", unsorted)
+  fmt.Println("sorted:", BubbleSort(unsorted))
+}
+
+func BubbleSort(input []int) []int {
+
+  for i := 0; i < len(input) - 1; i++ {
+    bubble(input, len(input) - 1 - i)
+  }
+  
+  return input
+}
+
+func bubble(input []int, index int) {
+  for i := 0; i < index; i++ {
+    if input[i] > input[i + 1] && i < len(input) - 1 {
+      tmp := input[i]
+      input[i] = input[i + 1]
+      input[i + 1] = tmp
+    }
+  }
+}
