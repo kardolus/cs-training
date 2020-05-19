b974668bc692354d66eed63076650f09a0a08243
Guillermo Kardolus <kardolus@gmail.com>
2020-05-19 12:30:42 -0400

    Touch up

--- builder.go
+++ builder.go
@@ -1,85 +1,85 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 /*
-  Think about a happy meal :). It consist of a main item, a side item, a drink, 
+  Think about a happy meal :). It consist of a main item, a side item, a drink,
   and a toy. The main item can be a chicken burger or a veggie burger or whatever.
   The drink can be a coke or a sprite etc. But the process of constructing a happy
   meal always consists of the same steps.
 */
 
-func main() {  
-  fmt.Println("Make me a happy meal!")
-  
-  var assembly HappyMeal
-  assembly = assembly.Side("Fries").Drink("Coke").(HappyMeal)
-  
-  fmt.Printf("Assembly: %+v\n", assembly)
-  
-  var choiceOne = assembly.Main("Veggie").Toy("Motorcycle").Build()
-  var choiceTwo = assembly.Main("BatBurder").Toy("Helicopter").Build()
- 
-  choiceOne.Eat()
-  choiceTwo.Eat()
+func main() {
+	fmt.Println("Make me a happy meal!")
+
+	var assembly HappyMeal
+	assembly = assembly.Side("Fries").Drink("Coke").(HappyMeal)
+
+	fmt.Printf("Assembly: %+v\n", assembly)
+
+	var choiceOne = assembly.Main("Veggie").Toy("Motorcycle").Build()
+	var choiceTwo = assembly.Main("BatBurder").Toy("Helicopter").Build()
+
+	choiceOne.Eat()
+	choiceTwo.Eat()
 }
 
 type Dinner interface {
-  Eat() 
+	Eat()
 }
 
 type Builder interface {
-  Build() Dinner
-  Main(choice string) Builder
-  Side(choice string) Builder
-  Drink(choice string) Builder
-  Toy(choice string) Builder
+	Build() Dinner
+	Main(choice string) Builder
+	Side(choice string) Builder
+	Drink(choice string) Builder
+	Toy(choice string) Builder
 }
 
 type SomeOrder struct {
-  YourOrder string
+	YourOrder string
 }
 
 func (s SomeOrder) Eat() {
-  fmt.Println("Eating:", s.YourOrder)
+	fmt.Println("Eating:", s.YourOrder)
 }
 
 type HappyMeal struct {
-  YourMain string
-  YourSide string
-  YourDrink string
-  YourToy string
+	YourMain  string
+	YourSide  string
+	YourDrink string
+	YourToy   string
 }
 
 func (h HappyMeal) Main(choice string) Builder {
-  fmt.Println("Your pick:", choice)
-  
-  h.YourMain = choice
-  
-  return h
+	fmt.Println("Your pick:", choice)
+
+	h.YourMain = choice
+
+	return h
 }
 
 func (h HappyMeal) Side(choice string) Builder {
-  fmt.Println("Your pick:", choice)
-  h.YourSide = choice
-  return h
+	fmt.Println("Your pick:", choice)
+	h.YourSide = choice
+	return h
 }
 
 func (h HappyMeal) Drink(choice string) Builder {
-  fmt.Println("Your pick:", choice)
-  h.YourDrink = choice
-  return h
+	fmt.Println("Your pick:", choice)
+	h.YourDrink = choice
+	return h
 }
 
 func (h HappyMeal) Toy(choice string) Builder {
-  fmt.Println("Your pick:", choice)
-  h.YourToy = choice
-  return h
+	fmt.Println("Your pick:", choice)
+	h.YourToy = choice
+	return h
 }
 
 func (h HappyMeal) Build() Dinner {
-  var meal = fmt.Sprintf("%s %s %s %s", h.YourMain, h.YourSide, h.YourDrink, h.YourToy)
-  return SomeOrder{YourOrder: meal}
+	var meal = fmt.Sprintf("%s %s %s %s", h.YourMain, h.YourSide, h.YourDrink, h.YourToy)
+	return SomeOrder{YourOrder: meal}
 }
--- queue.go
+++ queue.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
-func main() {  
-  var queue = NewQueue(2)
-  
-  fmt.Println("Empty()", queue.Empty())
-  fmt.Println("Size()", queue.Size())
-  fmt.Println("Contains(3)", queue.Contains(3))
-  fmt.Println("Peek() %+v", wrap(queue.Peek()))
-  fmt.Println("Poll() %+v", wrap(queue.Poll()))
-  
-  fmt.Println("----> Offer(3)")
-  queue.Offer(3)
-  
-  fmt.Println("Empty()", queue.Empty())
-  fmt.Println("Size()", queue.Size())
-  fmt.Println("Contains(3)", queue.Contains(3))
-  fmt.Println("Peek() %+v", wrap(queue.Peek()))
-  fmt.Println("Poll() %+v", wrap(queue.Poll()))
-  fmt.Println("Size()", queue.Size())
-  
-  fmt.Println("----> Offer(3)")
-  queue.Offer(3)
-  
-  fmt.Println("----> Offer(4)")
-  queue.Offer(4)
-  
-  fmt.Println("Contains(4)", queue.Contains(4))
-  fmt.Println("Peek() %+v", wrap(queue.Peek()))
-  fmt.Println("Size()", queue.Size())
-  fmt.Println("Poll() %+v", wrap(queue.Poll()))
-  fmt.Println("Size()", queue.Size())
-  fmt.Println("Contains(4)", queue.Contains(4))
-  fmt.Println("Contains(3)", queue.Contains(3))
+func main() {
+	var queue = NewQueue(2)
+
+	fmt.Println("Empty()", queue.Empty())
+	fmt.Println("Size()", queue.Size())
+	fmt.Println("Contains(3)", queue.Contains(3))
+	fmt.Println("Peek() %+v", wrap(queue.Peek()))
+	fmt.Println("Poll() %+v", wrap(queue.Poll()))
+
+	fmt.Println("----> Offer(3)")
+	queue.Offer(3)
+
+	fmt.Println("Empty()", queue.Empty())
+	fmt.Println("Size()", queue.Size())
+	fmt.Println("Contains(3)", queue.Contains(3))
+	fmt.Println("Peek() %+v", wrap(queue.Peek()))
+	fmt.Println("Poll() %+v", wrap(queue.Poll()))
+	fmt.Println("Size()", queue.Size())
+
+	fmt.Println("----> Offer(3)")
+	queue.Offer(3)
+
+	fmt.Println("----> Offer(4)")
+	queue.Offer(4)
+
+	fmt.Println("Contains(4)", queue.Contains(4))
+	fmt.Println("Peek() %+v", wrap(queue.Peek()))
+	fmt.Println("Size()", queue.Size())
+	fmt.Println("Poll() %+v", wrap(queue.Poll()))
+	fmt.Println("Size()", queue.Size())
+	fmt.Println("Contains(4)", queue.Contains(4))
+	fmt.Println("Contains(3)", queue.Contains(3))
 }
 
 func wrap(vs ...interface{}) []interface{} {
-    return vs
+	return vs
 }
 
 /*
@@ -47,62 +47,62 @@ func wrap(vs ...interface{}) []interface{} {
 */
 
 type Queue struct {
-  data []int
-  tail int
-  head int
+	data []int
+	tail int
+	head int
 }
 
 func NewQueue(capacity int) Queue {
-  return Queue{
-    data: make([]int, capacity),
-  }
+	return Queue{
+		data: make([]int, capacity),
+	}
 }
 
 func (q *Queue) Empty() bool {
-  return q.tail == 0
+	return q.tail == 0
 }
 
 func (q *Queue) Size() int {
-  return q.tail - q.head
+	return q.tail - q.head
 }
 
 func (q *Queue) Offer(value int) {
-  q.copy()
-  q.data[q.tail] = value
-  q.tail++ 
+	q.copy()
+	q.data[q.tail] = value
+	q.tail++
 }
 
 func (q *Queue) Contains(value int) bool {
-  for i := q.head; i < q.tail; i++ {
-    if value == q.data[i] {
-      return true
-    }
-  }
-  return false
+	for i := q.head; i < q.tail; i++ {
+		if value == q.data[i] {
+			return true
+		}
+	}
+	return false
 }
 
 func (q *Queue) Peek() (int, error) {
-  if q.tail - q.head == 0 {
-    return 0, fmt.Errorf("the queue is empty")
-  }
-  return q.data[q.head], nil
+	if q.tail-q.head == 0 {
+		return 0, fmt.Errorf("the queue is empty")
+	}
+	return q.data[q.head], nil
 }
 
 func (q *Queue) Poll() (int, error) {
-  result, err := q.Peek()
-  if err != nil {
-    return 0, err
-  }
-  
-  q.head++
-  
-  return result, nil
+	result, err := q.Peek()
+	if err != nil {
+		return 0, err
+	}
+
+	q.head++
+
+	return result, nil
 }
 
 func (q *Queue) copy() {
-  if q.tail == len(q.data) {
-    newData := make([]int, q.tail*2)
-    copy(newData, q.data)
-    q.data = newData
-  }
+	if q.tail == len(q.data) {
+		newData := make([]int, q.tail*2)
+		copy(newData, q.data)
+		q.data = newData
+	}
 }
